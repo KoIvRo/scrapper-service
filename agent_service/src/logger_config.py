7aadb26d21a02eefb2584fba69b999fa0f694624
KoIvRo <korot4ev29@gmail.com>
2026-05-21 21:32:22 +1000

    feat: logger в agent_service

---
+++ agent_service/src/logger_config.py
@@ -0,0 +1,26 @@
+import logging
+from pythonjsonlogger.json import JsonFormatter
+from config import settings
+
+
+def init_logger() -> None:
+    """Настройка логгера ВЫЗЫВАТЬ ПРИ СТАРТЕ."""
+    logger = logging.getLogger()
+    formatter = JsonFormatter(
+        "%(asctime)s %(levelname)s %(name)s %(message)s", datefmt="%Y-%m-%d %H:%M:%S"
+    )
+
+    log_handler: logging.Handler
+
+    if settings.logger.output == "stdout":
+        log_handler = logging.StreamHandler()
+    else:
+        log_handler = logging.FileHandler(settings.logger.output, encoding="utf-8")
+
+    log_handler.setFormatter(formatter)
+    logger.addHandler(log_handler)
+    logger.setLevel(settings.logger.level)
+
+    logging.getLogger("aiogram").setLevel(logging.WARNING)
+    logging.getLogger("aiohttp").setLevel(logging.WARNING)
+    
--- agent_service/src/main.py
+++ agent_service/src/main.py
@@ -1,5 +1,7 @@
 from config import settings
+from logger_config import init_logger
 
 
 if __name__ == "__main__":
+    init_logger()
     print(settings.kafka.topic)
